29ca1ba3f25cb272256b15e664678898dd8ee8fc
Ivan Morozov <morozov_i@bezlimit.ru>
2022-07-26 19:06:50 +0300

    всякое-разное

---
+++ tests_store_prod/users/test_personal_manager.py
@@ -0,0 +1,56 @@
+import requests
+
+
+url = 'https://api.store.dev.bezlimit.ru/v2/users/personal-manager'
+headers = {
+    'Authorization': 'Basic YXBpU3RvcmU6VkZ6WFdOSmhwNTVtc3JmQXV1dU0zVHBtcnFTRw==',
+    'accept': 'application/json'
+}
+
+
+class TestNegative:
+    def test_unauthorized(self):
+        response = requests.get(url, headers=headers)
+
+        assert response.status_code == 401
+        assert response.json() == {
+            "name": "Unauthorized",
+            "message": "Your request was made with invalid credentials.",
+            "code": 0,
+            "status": 401,
+            "type": "yii\\web\\UnauthorizedHttpException"
+        }
+
+
+class TestPositive:
+
+    token0 = 'Kyu8SyaqOyCQCOBTJQ93580ig_xLh1UsU2JS2i07Tt5WnJ9tc6XGqXHlrDXEUyiH'
+    token1 = 'jxrOOpE33Zb944m8w5KUXhgIHPGHS1V0zO1wbphFnXNZSjL-Sa5_KGwYwndejafJ'
+    token2 = 'plq-rqmPaSlZ1bpN-LFYZX_WMQOjiuWVK8-2WnUG8n-AyBhprQgSVdXfE58Al9nW'
+
+    def test_correct_level_0(self):
+        headers.update({'Api-Token': self.token0})
+
+        response = requests.get(url, headers=headers)
+
+        assert response.json() is None
+
+    def test_correct_level_1(self):
+        headers.update({'Api-Token': self.token1})
+
+        response = requests.get(url, headers=headers)
+
+        assert response.json() == {
+            "name": "Test AutoInp0",
+            "phone": 9686244447
+        }
+
+    def test_correct_level_2(self):
+        headers.update({'Api-Token': self.token2})
+
+        response = requests.get(url, headers=headers)
+
+        assert response.json() == {
+            "name": "тест Рахимов",
+            "phone": 9682229543
+        }
---
+++ tests_store_prod/users/test_restore_password.py
@@ -0,0 +1,89 @@
+import requests
+
+
+url = 'https://api.store.dev.bezlimit.ru/v2/users/restore-password'
+email = 'api_store_autotest@bezlimit.ru'
+headers = {
+    'Authorization': 'Basic YXBpU3RvcmU6VkZ6WFdOSmhwNTVtc3JmQXV1dU0zVHBtcnFTRw==',
+    'accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+class TestValidation:
+    def test_empty_params(self):
+        params = {
+            'email': None,
+            'phone': None,
+            'code': None
+        }
+
+        response = requests.put(url, headers=headers, params=params)
+
+        assert response.status_code == 422
+        assert response.json() == [{'field': 'email', 'message': 'Укажите email'}]
+
+    def test_email_integer(self):
+        params = {
+            'email': 123456,
+            'phone': None,
+            'code': None
+        }
+
+        response = requests.put(url, headers=headers, data=params)
+
+        assert response.status_code == 422
+        assert response.json() == [{'field': 'email', 'message': 'Указанный email не найден в системе'}]
+
+    def test_phone_small(self):
+        params = {
+            'email': None,
+            'phone': 969658,
+            'code': None
+        }
+
+        response = requests.put(url, headers=headers, data=params)
+
+        assert response.status_code == 422
+        assert response.json() == [{'field': 'phone', 'message': 'Указанный номер телефона не найден в системе'}]
+
+    def test_phone_large(self):
+        params = {
+            'email': None,
+            'phone': 9000000000000000000,
+            'code': None
+        }
+
+        response = requests.put(url, headers=headers, data=params)
+
+        assert response.status_code == 422
+        assert response.json() == [{'field': 'phone', 'message': 'Указанный номер телефона не найден в системе'}]
+
+class TestNegative:
+    def test_email_incorrect(self):
+        params = {
+            'email': 'billy@herrington.gm',
+            'phone': None,
+            'code': None
+        }
+
+        response = requests.put(url, headers=headers, data=params)
+
+        assert response.status_code == 422
+        assert response.json() == [{'field': 'email', 'message': 'Указанный email не найден в системе'}]
+
+    def test_phone_incorrect(self):
+        params = {
+            'email': None,
+            'phone': 9000000000,
+            'code': None
+        }
+
+        response = requests.put(url, headers=headers, data=params)
+
+        assert response.status_code == 422
+        assert response.json() == [{'field': 'phone', 'message': 'Указанный номер телефона не найден в системе'}]
+
+
+class TestPositive:
+    def test_anal(self):
+        pass
