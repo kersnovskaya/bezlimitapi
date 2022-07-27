b88f89ef3e6a73ca88bb431dcff530f795595c22
Ivan Morozov <morozov_i@bezlimit.ru>
2022-07-27 16:36:12 +0300

    всякое-разное

--- tests_dev/account_phone/test_post_phone.py
+++ tests_dev/account_phone/test_post_phone.py
@@ -1,5 +1,4 @@
 import requests
-import pytest_check as check
 
 token = 'NEKTX5ZvPNovEEmkL-8tKxcPJBuCx16v5sQCox8b483zOvfEsCwcSwrjicpWDqDI'
 url = 'https://lktest.bezlimit.ru/v1/account/phone'
@@ -22,18 +21,18 @@ class Test:
 
         response = requests.post(url, headers=self.headers, data=data)
 
-        check.equal(response.status_code, 201, f'Код ответа "{response.status_code}", а не "201".')
-        check.equal(response.reason, 'Created', f'Результат запроса "{response.reason}", а не "Created".')
+        assert response.status_code == 201, f'Код ответа "{response.status_code}", а не "201".'
+        assert response.reason == 'Created', f'Результат запроса "{response.reason}", а не "Created".'
 
     def test_empty_phone(self):
         data = {}
 
         response = requests.post(url, headers=self.headers, data=data)
 
-        check.equal(response.status_code, 422, f'Код ответа "{response.status_code}", а не "422".')
-        check.equal(response.reason, 'Data Validation Failed.')
-        check.equal(response.json()[0]['field'], 'phone')
-        check.equal(response.json()[0]['message'], 'Необходимо заполнить «Номер телефона».')
+        assert response.status_code == 422, f'Код ответа "{response.status_code}", а не "422".'
+        assert response.reason == 'Data Validation Failed.'
+        assert response.json()[0]['field'] == 'phone'
+        assert response.json()[0]['message'] == 'Необходимо заполнить «Номер телефона».'
 
     def test_long_name(self):
         name = 'test_validation_too_long_name_1'  # 31 chars
@@ -42,17 +41,17 @@ class Test:
 
         response = requests.post(url, headers=self.headers, data=data)
 
-        check.equal(response.status_code, 422, 'Код ответа "{}", а не "422"'.format(response.status_code))
-        check.equal(response.reason, 'Data Validation Failed.')
-        check.equal(response.json()[0]['field'], 'name')
-        check.equal(response.json()[0]['message'], 'Значение «Название телефона» должно содержать максимум 30 символов.')
+        assert response.status_code == 422, 'Код ответа "{}", а не "422"'.format(response.status_code)
+        assert response.reason == 'Data Validation Failed.'
+        assert response.json()[0]['field'] == 'name'
+        assert response.json()[0]['message'] == 'Значение «Название телефона» должно содержать максимум 30 символов.'
 
     def test_unauthorized(self):
         response = requests.post(url)
 
-        check.equal(response.status_code, 401, f'Код ответа "{response.status_code}", а не "401".')
-        check.equal(response.reason, 'Unauthorized')
-        check.equal(response.json()['message'], 'Your request was made with invalid credentials.')
+        assert response.status_code == 401, f'Код ответа "{response.status_code}", а не "401".'
+        assert response.reason, 'Unauthorized'
+        assert response.json()['message'], 'Your request was made with invalid credentials.'
 
     def test_incorrect_phone(self):
         data = {
@@ -60,13 +59,11 @@ class Test:
         }
         response = requests.post(url, headers=self.headers, data=data)
 
-        check.equal(response.status_code, 422, f'Код ответа "{response.status_code}", а не "422".')
-        check.equal(response.reason, 'Data Validation Failed.')
-        check.equal(
-            response.json(),
-            [{'field': 'phone', 'message': 'Введите номер телефона в формате 9001112233.'}],
+        assert response.status_code == 422, f'Код ответа "{response.status_code}", а не "422".'
+        assert response.reason == 'Data Validation Failed.'
+        assert response.json() == [{'field': 'phone', 'message': 'Введите номер телефона в формате 9001112233.'}],\
             f'Ошибка в теле ответа: "{response.json()}"'
-        )
+
 
     def test_not_bezlimit_phone(self):
         data = {
@@ -76,13 +73,10 @@ class Test:
         response = requests.post(url, headers=self.headers, data=data)
         print('\n', response.json())
 
-        check.equal(response.status_code, 422, f'Код ответа "{response.status_code}", а не "422".')
-        check.equal(response.reason, 'Data Validation Failed.')
-        check.equal(
-            response.json(),
-            [{'field': 'phone', 'message': 'Введенный номер не обслуживается в Безлимит!'}],
+        assert response.status_code == 422, f'Код ответа "{response.status_code}", а не "422".'
+        assert response.reason == 'Data Validation Failed.'
+        assert response.json() == [{'field': 'phone', 'message': 'Введенный номер не обслуживается в Безлимит!'}],\
             f'Ошибка в теле ответа: "{response.json()}"'
-        )
 
     def test_number_connected(self):
         data = {
@@ -91,10 +85,7 @@ class Test:
 
         response = requests.post(url, headers=self.headers, data=data)
 
-        check.equal(response.status_code, 422, f'Код ответа "{response.status_code}", а не "422".')
-        check.equal(response.reason, 'Data Validation Failed.')
-        check.equal(
-            response.json(),
-            [{'field': 'phone', 'message': 'Номер телефона уже добавлен в аккаунт и подтвержден'}],
+        assert response.status_code == 422, f'Код ответа "{response.status_code}", а не "422".'
+        assert response.reason == 'Data Validation Failed.'
+        assert response.json() == [{'field': 'phone', 'message': 'Номер телефона уже добавлен в аккаунт и подтвержден'}],\
             f'Ошибка в теле ответа: "{response.json()}"'
-        )
